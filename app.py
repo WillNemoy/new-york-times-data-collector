b60f61e35d0864bac7f70578fd985333ed3a0562
Will Nemoy <98560701+WillNemoy@users.noreply.github.com>
2022-11-13 23:24:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pynytimes import NYTAPI
+from pprint import pprint
 
 import os
 from dotenv import load_dotenv
@@ -9,14 +10,14 @@ load_dotenv() # look in the ".env" file for env vars
 
 API_KEY = os.getenv("NY_API_KEY")
 
-nyt = NYTAPI("2", parse_dates=True)
+nyt = NYTAPI(API_KEY, parse_dates=True)
 
 articles = nyt.article_search(
-    query = "Obama",
-    results = 30,
+    query = "Amazon",
+    results = 1,
     dates = {
-        "begin": datetime.datetime(2019, 1, 31),
-        "end": datetime.datetime(2019, 2, 28)
+        "begin": datetime.datetime(2022, 1, 31),
+        "end": datetime.datetime(2022, 2, 28)
     },
     options = {
         "sort": "oldest",
@@ -26,13 +27,10 @@ articles = nyt.article_search(
             "Reuters",
             "International Herald Tribune"
         ],
-        "news_desk": [
-            "Politics"
-        ],
         "type_of_material": [
-            "News Analysis"
+            "News"
         ]
     }
 )
 
-print(articles)
+pprint(articles)
